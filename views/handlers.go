3726a3d7acb370cdaf29ab2a26e155dfaa0d6c80
Khryzen <mdeguzman.cpe@gmail.com>
2024-09-19 17:02:02 +0800

    Add UI interface

--- views/handlers.go
+++ views/handlers.go
@@ -3,17 +3,25 @@ package views
 import (
 	"net/http"
 	"strings"
+
+	"github.com/uadmin/uadmin"
 )
 
 func PageHandlers(w http.ResponseWriter, r *http.Request) {
 	page := strings.TrimPrefix(r.URL.Path, "/")
 	page = strings.TrimSuffix(page, "/")
 
+	uadmin.Trail(uadmin.DEBUG, "page: %s", page)
+
 	context := map[string]interface{}{}
 
 	switch page {
 	case "dashboard":
 		context = DashboardHandler(w, r)
+		page = "dashboard"
+	default:
+		context = DashboardHandler(w, r)
+		page = "dashboard"
 	}
 	WebRender(w, r, page, context)
 }
