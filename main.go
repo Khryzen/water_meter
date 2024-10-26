e13cc6e0b3ec5d6f895defd6138b8e489c129570
Khryzen <mdeguzman.cpe@gmail.com>
2024-10-26 14:26:04 +0800

    Add business logic for the devices page

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"net/http"
 
+	"github.com/mbdeguzman/water_district/models"
 	"github.com/mbdeguzman/water_district/views"
 	"github.com/uadmin/uadmin"
 )
 
 func main() {
-	uadmin.Register()
+	uadmin.Register(
+		models.Bill{},
+		models.BillingPeriod{},
+		models.Client{},
+		models.Device{},
+		models.Payment{},
+		models.Reading{},
+	)
 
 	http.HandleFunc("/", uadmin.Handler(views.PageHandlers))
 	uadmin.Port = 8080
--- views/devices.go
+++ views/devices.go
@@ -1,10 +1,17 @@
 package views
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mbdeguzman/water_district/models"
+	"github.com/uadmin/uadmin"
+)
 
 func DevicesHandler(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	context := map[string]interface{}{}
-
+	devices := []models.Device{}
+	uadmin.All(&devices)
+	context["Devices"] = devices
 	context["Title"] = "Water District | Devices"
 	return context
 }
