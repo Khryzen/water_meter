0ca4e5c11df63dc03e27a5fd177267e89cffd34c
Khryzen <mdeguzman.cpe@gmail.com>
2024-11-04 16:46:19 +0800

    Autogenerated bill

--- models/bill.go
+++ models/bill.go
@@ -5,20 +5,36 @@ import (
 	"time"
 
 	"github.com/uadmin/uadmin"
+	"golang.org/x/exp/rand"
 )
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 type Bill struct {
 	uadmin.Model
 	ReferenceNumber string
+	Client          Client
+	ClientID        uint
 	AccountNumber   string
 	BillingPeriod   BillingPeriod
 	BillingPeriodID uint
-	Reading         Reading
-	ReadingID       uint
 	Amount          float64
 	DueDate         time.Time
 }
 
 func (b *Bill) String() string {
 	return b.AccountNumber + "- Php " + fmt.Sprint(b.Amount)
+}
+
+func (b *Bill) Save() {
+	b.ReferenceNumber = GenerateRandomString(10)
+	uadmin.Save(b)
+}
+
+func GenerateRandomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
--- models/billing_period.go
+++ models/billing_period.go
@@ -15,3 +15,23 @@ type BillingPeriod struct {
 }
 
 // Create a save function that will generate the bills
+func (b *BillingPeriod) Save() {
+	uadmin.Save(b)
+
+	clients := []Client{}
+	uadmin.Filter(&clients, "active = ?", true)
+
+	for i := range clients {
+		readings := []Reading{}
+		uadmin.Filter(&readings, "month = ? AND year = ? AND client_id = ?", b.Month, b.Year, clients[i].ID)
+		bill := Bill{}
+		bill.ClientID = clients[i].ID
+		bill.BillingPeriodID = b.ID
+		consumption := 0.00
+		for j := range readings {
+			consumption += readings[j].Consumption
+		}
+		bill.Amount = b.Multiplier * consumption
+		bill.Save()
+	}
+}
