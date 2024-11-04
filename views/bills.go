3f39bf41a2de2a1953a9dd9fbff805d4907a4d4d
Khryzen <mdeguzman.cpe@gmail.com>
2024-11-04 16:50:53 +0800

    Bills template

--- views/bills.go
+++ views/bills.go
@@ -1,10 +1,19 @@
 package views
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mbdeguzman/water_district/models"
+	"github.com/uadmin/uadmin"
+)
 
 func BillHandler(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	context := map[string]interface{}{}
 
+	bills := []models.Bill{}
+	uadmin.All(&bills)
+
+	context["Bills"] = bills
 	context["Title"] = "Water District | Bills"
 	return context
 }
