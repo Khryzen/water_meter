9dc5948a1251db93e928e57467a3f3b1acb5032d
Khryzen <mdeguzman.cpe@gmail.com>
2024-11-05 13:12:54 +0800

    Dashboard basic information

--- views/dashboard.go
+++ views/dashboard.go
@@ -1,10 +1,21 @@
 package views
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mbdeguzman/water_district/models"
+	"github.com/uadmin/uadmin"
+)
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	context := map[string]interface{}{}
 
+	devices := models.Device{}
+	bill := models.Bill{}
+
+	context["Bill"] = uadmin.Count(&bill, "paid = ?", false)
+	context["Paid"] = uadmin.Count(&bill, "paid = ?", true)
+	context["Device"] = uadmin.Count(&devices, "active = ?", true)
 	context["Title"] = "Water District | Dashboard"
 	return context
 }
